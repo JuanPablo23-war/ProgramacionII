e47a2a7d59da0793ac4537d8da19028dbb6689d1
juan pablo <ariassanabriajuanpablo@gmail.com>
2026-08-17 11:46:25 -0500

    intenacionalizacion

--- ConfigProperties/src/co/edu/uptc/presenter/Presenter.java
+++ ConfigProperties/src/co/edu/uptc/presenter/Presenter.java
@@ -1,45 +1,67 @@
 package co.edu.uptc.presenter;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import co.edu.uptc.model.Calculator;
 import co.edu.uptc.view.IOManager;
 
 public class Presenter {
     private Calculator calculator;
     private IOManager ioManager;
+    private ResourceBundle messages;
 
     public Presenter() {
-        this.calculator = new Calculator();
-        this.ioManager = new IOManager();
+        calculator = new Calculator();
+        ioManager = new IOManager();
+        messages = ResourceBundle.getBundle("greeting", Locale.getDefault());  
+    }
+
+    public void changeLanguage() {
+        String[] options = {messages.getString("espanol"),messages.getString("ingles"),messages.getString("frances") };
+        int choice = ioManager.showOptionDialog(messages.getString("mensaje_cambiar_idioma"), options);;
+        Locale locale = Locale.getDefault();
+        switch (choice){
+            case 1 -> locale = Locale.ENGLISH;
+            case 2 -> locale = Locale.FRENCH;
+        }
+        messages = ResourceBundle.getBundle("greeting", locale);
     }
 
     public void menu() {
-        String[] options = {"Add", "Subtract", "Multiply", "Divide", "Exit"};
+        String[] options = {messages.getString("texto_sumar"),
+            messages.getString("texto_restar"), 
+            messages.getString("texto_multiplicar"),
+            messages.getString("texto_dividir"), 
+            messages.getString("texto_salir"),
+            messages.getString("texto_cambiar_idioma")};
         int choice = -1;
 
         while (choice != 4) {
-            choice = ioManager.showOptionDialog("Elige una operación:", options);
+            choice = ioManager.showOptionDialog(messages.getString("mensaje_operacion"), options);
             if (choice == 4) {
                 break;
             }
-            int a = Integer.parseInt(ioManager.showInputDialog("Ingresar el primer número:"));
-            int b = Integer.parseInt(ioManager.showInputDialog("Ingresar el segundo número:"));
+            int a = Integer.parseInt(ioManager.showInputDialog(messages.getString("mensaje_ingresar_numero1")));
+            int b = Integer.parseInt(ioManager.showInputDialog(messages.getString("mensaje_ingresar_numero2")));
             switch (choice) {
                 case 0:
-                    ioManager.showMessage("Resultado: " + calculator.add(a, b));
+                    ioManager.showMessage(messages.getString("lblRes") + calculator.add(a, b));
                     break;
                 case 1:
-                    ioManager.showMessage("Resultado: " + calculator.subtract(a, b));
+                    ioManager.showMessage(messages.getString("lblRes") + calculator.subtract(a, b));
                     break;
                 case 2:
-                    ioManager.showMessage("Resultado: " + calculator.multiply(a, b));
+                    ioManager.showMessage(messages.getString("lblRes") + calculator.multiply(a, b));
                     break;
                 case 3:
                     try {
-                        ioManager.showMessage("Resultado: " + calculator.divide(a, b));
+                        ioManager.showMessage(messages.getString("lblRes") + calculator.divide(a, b));
                     } catch (IllegalArgumentException e) {
                         ioManager.showMessage(e.getMessage());
                     }
                     break;
+                case 4: 
+                    changeLanguage();
             }
         }
     }
